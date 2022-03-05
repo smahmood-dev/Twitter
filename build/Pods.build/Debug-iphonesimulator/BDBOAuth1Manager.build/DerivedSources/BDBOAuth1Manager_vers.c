125bc306d0683aa896b1ae02cb0db5b801896049
smahmood99 <76938013+smahmood99@users.noreply.github.com>
2022-03-05 17:29:50 -0600

    Initial Commit

---
+++ build/Pods.build/Debug-iphonesimulator/BDBOAuth1Manager.build/DerivedSources/BDBOAuth1Manager_vers.c
@@ -0,0 +1,5 @@
+ extern const unsigned char BDBOAuth1ManagerVersionString[];
+ extern const double BDBOAuth1ManagerVersionNumber;
+
+ const unsigned char BDBOAuth1ManagerVersionString[] __attribute__ ((used)) = "@(#)PROGRAM:BDBOAuth1Manager  PROJECT:Pods-1" "\n";
+ const double BDBOAuth1ManagerVersionNumber __attribute__ ((used)) = (double)1.;
